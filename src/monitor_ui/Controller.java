50bd60fd248f4b7fb76a3d38ce701e6f201e2129
totenhund <caligoyan@gmail.com>
2021-02-09 17:19:36 +0600

    Draft ui

---
+++ src/monitor_ui/Controller.java
@@ -0,0 +1,35 @@
+package monitor_ui;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Controller {
+
+    @FXML
+    public void alertExample(){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION, "Button clicked");
+        alert.showAndWait();
+    }
+
+    @FXML
+    public void openStats(){
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource("stats.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Stats");
+            stage.setScene(new Scene(root));
+            stage.show();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/monitor_ui/Main.java
@@ -0,0 +1,28 @@
+package monitor_ui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Linux Monitor");
+
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("sample.fxml"));
+        Controller controller = loader.getController();
+
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
