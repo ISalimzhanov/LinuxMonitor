bdf4374d57907834863c4e2bfa3d87710a7a9509
totenhund <caligoyan@gmail.com>
2021-02-18 17:06:55 +0600

    db draft

--- src/main/java/database/LocalDatabase.java
+++ src/main/java/database/LocalDatabase.java
@@ -1,22 +1,37 @@
 package database;
 
 
-public class LocalDatabase {
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 
-    private static LocalDatabase instance;
+import javax.print.Doc;
+import java.util.HashMap;
+
+public class LocalDatabase  {
+
+    private static MongoDatabase instance;
 
     private LocalDatabase(){}
 
-    public static LocalDatabase getInstance(){
+    public static MongoDatabase getInstance(){
         if (instance == null){
-            instance = new LocalDatabase();
+            instance = MongoClients.create().getDatabase("procdb");
         }
 
         return instance;
     }
 
-    public void addData(){
-
+    public static void addData(HashMap<String, String> procDiary){
+        Document doc = new Document("name", "process")
+                .append("proc_name", procDiary.get("proc_name"))
+                .append("cpu", procDiary.get("cpu"))
+                .append("time", procDiary.get("time"));
+        MongoCollection<Document> collection = instance.getCollection("processes");
+        collection.insertOne(doc);
+        System.out.println(instance.getCollection("processes").countDocuments());
     }
 
     public int getData(){
--- src/main/java/monitor_ui/Controller.java
+++ src/main/java/monitor_ui/Controller.java
@@ -1,5 +1,7 @@
 package monitor_ui;
 
+import com.mongodb.client.MongoDatabase;
+import database.LocalDatabase;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -8,6 +10,7 @@ import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 public class Controller {
 
@@ -25,6 +28,12 @@ public class Controller {
             Stage stage = new Stage();
             stage.setTitle("Stats");
             stage.setScene(new Scene(root));
+            MongoDatabase db = LocalDatabase.getInstance();
+            HashMap<String, String> hm = new HashMap<String, String>();
+            hm.put("proc_name", "lol");
+            hm.put("cpu", "90");
+            hm.put("time", "102");
+            LocalDatabase.addData(hm);
             stage.show();
         }
         catch (IOException e) {
